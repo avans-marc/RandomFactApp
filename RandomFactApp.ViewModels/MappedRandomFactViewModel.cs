5fa60b96396cc6199fda0b95d27f24d700412196
Marc Mathijssen <151033279+avans-marc@users.noreply.github.com>
2024-11-26 19:10:51 +0100

    Added Map Feature + Version Bumpback (#2)
    * Initial commit with readme
    
    * Added alternative implementation to demonstrate the advantage of DI.
    
    * Added Union Architecture  & Unit Tests (#1)
    
    * Setup Unit Tests and create separate physical libraries to make it more testable.
    
    * WIP - testing
    
    * Union + Unit Testing Finish
    
    * Example of MVVM binding to a Map Control
    
    * Example of MVVM binding to a Map Control
    
    * Removed secret
    
    * Removed secret
    
    * Removed secret
    
    * Bump back to version 8 because of dotnet/android/issues/9133

---
+++ RandomFactApp.ViewModels/MappedRandomFactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomFactApp.ViewModels
+{
+    public partial class MappedRandomFactViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string fact;
+
+        [ObservableProperty]
+        private Location location;
+
+        public MappedRandomFactViewModel(string fact, Location location)
+        {
+            this.fact = fact;
+            this.location = location;
+        }
+    }
+}
